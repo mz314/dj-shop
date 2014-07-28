79a963afd0c2243a7bcb404ecf70dfd5e0c95074
Maciej Zakrzewski <maciej.zakrzewski@polcode.net>
2014-07-28 15:39:27 +0200

    home article list

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,7 +2,9 @@ var djShopControllers = angular.module('djShopControllers', []);
 
 
 
-djShopControllers.controller('HomeCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-   // $scope.phoneId = $routeParams.phoneId;
+djShopControllers.controller('HomeCtrl', ['$scope', '$http',
+  function($scope, $http) {
+        $http.get('ajax/fronts').success(function(data) {
+         $scope.articles = data;
+    });
   }]);
